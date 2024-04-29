cf35a0c95f950b8f49e6d89c607175f6ca418b4d
Vladislav Kozlovski <Valdismin@gmail.com>
2024-04-29 17:03:06 +0200

    init

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,14 @@
 import "./header.scss"
 import Image from "next/image";
+import telegram from "../../../public/telegram.svg";
+import viber from "../../../public/viber.svg";
+import logo from "../../../public/logo2.png";
 
 export const Header = () => {
     return (
         <header className="header-wrapper">
             <div className='container'>
-                <Image src="/logo2.png" alt="Logo" width={200} height={80} className="header-logo"/>
+                <Image src={logo} alt="Logo" width={200} height={80} className="header-logo"/>
                 <nav>
                     <ul className="navigation-links-wrapper">
                         <li className="link">
@@ -24,8 +27,8 @@ export const Header = () => {
                 </nav>
                 <div className="contacts-wrapper">
                     <div className="icons-wrapper">
-                        <Image src="/public/telegram.svg" alt="Phone" width={30} height={30} className="icon"/>
-                        <Image src="/viber.svg" alt="Phone" width={30} height={30} className="icon"/>
+                        <Image src={telegram} alt="Phone" width={30} height={30} className="icon"/>
+                        <Image src={viber} alt="Phone" width={30} height={30} className="icon"/>
                     </div>
                     <span className="phone-number">+375-29-194-08-94</span>
                 </div>
